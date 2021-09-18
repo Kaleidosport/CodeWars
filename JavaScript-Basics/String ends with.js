b6387ba0d3a4b33f8faf8cafd8614a671cc3cb85
Kaleidosport <lambert.ant14@gmail.com>
2021-09-18 11:22:30 +0200

    Adding 7th exercise

---
+++ JavaScript-Basics/String ends with.js
@@ -0,0 +1,7 @@
+// Origin: https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+
+function solution(str, ending){
+    return str.endsWith(ending);
+  }
+
+// New method acquired: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
