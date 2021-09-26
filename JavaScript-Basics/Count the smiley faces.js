fdb427386dd4a568dfb2b5a0b66a7903e9e2db37
Kaleidosport <lambert.ant14@gmail.com>
2021-09-26 11:24:48 +0200

    Added comments to 14th exercise

--- JavaScript-Basics/Count the smiley faces.js
+++ JavaScript-Basics/Count the smiley faces.js
@@ -1,15 +1,18 @@
-// Origin: 
+// Origin: https://www.codewars.com/kata/583203e6eb35d7980400002a
 
 //return the total number of smiling faces in the array
 function countSmileys(arr) {
     let validSmileys = [];
+    // or let counter = 0;
     for (i = 0; i < arr.length; i++) {
       if (/(\:|\;)(\~|\-)?(\)|[D])/.test(arr[i])) {
           // Don't have to specify the {amount} of chars when written down this way
         validSmileys.push(arr[i]);
+        // or counter++;
       }
     }
     return validSmileys.length;
+    // or return counter;
   }
 
 /* RegEx was key here to make use of .test() but there's an even cleaner solution with arr.filter():
