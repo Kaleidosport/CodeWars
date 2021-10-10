777dd2de08123743b7247b85fcbeef02bffb5548
Kaleidosport <lambert.ant14@gmail.com>
2021-10-10 17:39:15 +0200

    Adding 22nd exercise

--- JavaScript-Basics/Replace with Alphabet Position.js
+++ JavaScript-Basics/Replace with Alphabet Position.js
@@ -1,4 +1,4 @@
-// Origin:
+// Origin: https://www.codewars.com/kata/546f922b54af40e1e90001da
 
 function alphabetPosition(text) {
     let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
