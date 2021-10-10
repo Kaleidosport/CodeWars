15f1af8918134c7adf08d6a67248dbf06e0ebea5
Kaleidosport <lambert.ant14@gmail.com>
2021-10-10 17:38:15 +0200

    Adding 22nd exercise

---
+++ JavaScript-Basics/Replace with Alphabet Position.js
@@ -0,0 +1,21 @@
+// Origin:
+
+function alphabetPosition(text) {
+    let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
+                   "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    text = text.toLowerCase()
+    let numericAlphabet = []
+
+    for (let i = 0; i < alphabet.length; i++) {
+      if (alphabet.indexOf(text[i]) > -1) numericAlphabet.push(alphabet.indexOf(text[i]) + 1)
+    }
+
+    return numericAlphabet.join(" ")
+}
+
+/** That one left a strong impression, tho:
+ * function alphabetPosition(text) {
+    return [...text].map(a => parseInt(a, 36) - 9).filter(a => a > 0).join(" ");
+   }
+ * Inspired by https://stackoverflow.com/questions/41427029/replace-a-letter-with-its-alphabet-position
+ */
