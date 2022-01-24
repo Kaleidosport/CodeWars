da18766577de496c4efa32ce29434175ef5e6a3e
Kaleidosport <lambert.ant14@gmail.com>
2022-01-24 21:21:52 +0100

    Adding 40th kata, edited 39th kata's link

--- JavaScript Fundamentals/Categorize New Member.js
+++ JavaScript Fundamentals/Categorize New Member.js
@@ -1,8 +1,8 @@
-// Origin:
+// Origin: https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa
 
 function openOrSenior(data){
     return data.map((a, b) => a[0] > 54 && a[1] > 7 ? "Senior" : "Open")
-}
+} // a[0] === data[x][0] & a[1] === data[x][1]
 
 // ***************** Same method, other syntax *******************
 
