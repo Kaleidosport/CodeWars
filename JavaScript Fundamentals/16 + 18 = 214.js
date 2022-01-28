7b91c0f53c1c974e2c2b66148a90636ab4b255c2
Kaleidosport <lambert.ant14@gmail.com>
2022-01-28 17:51:09 +0100

    Adding 43rd kata

---
+++ JavaScript Fundamentals/16 + 18 = 214.js
@@ -0,0 +1,8 @@
+// Origin: https://www.codewars.com/kata/5effa412233ac3002a9e471d
+
+function add(num1, num2) {
+    let sum = []
+    if (num1.toString().length <= num2.toString().length) [...num2.toString()].reverse().map((a, b) => sum.push(Number(a) + Number([...num1.toString()].reverse()[b] || 0)))
+    else [...num1.toString()].reverse().map((a, b) => sum.push(Number(a) + Number([...num2.toString()].reverse()[b] || 0)))
+    return Number(sum.reverse().join(""))
+}
